97ec2388f5e09842568886c262751e0c78b5d431
d4n-sec <d4n.for.sec@gmail.com>
2024-05-08 16:46:44 +0800

    init

---
+++ gopocs/scanner.go
@@ -0,0 +1,166 @@
+package gopocs
+
+import (
+	"dddd/structs"
+	"github.com/projectdiscovery/gologger"
+	"github.com/projectdiscovery/nuclei/v3/pkg/output"
+	"reflect"
+	"strings"
+	"sync"
+)
+
+var Mutex = &sync.Mutex{}
+
+// 单线程的
+var currentCount = 0
+
+func AddScan(scantype string, info structs.HostInfo, ch *chan struct{}, wg *sync.WaitGroup) {
+	currentCount += 1
+	if currentCount%100 == 0 {
+		gologger.Info().Msgf("[GoPoc] 当前进度: %v %v [%v/%v]", scantype, info.Host+":"+info.Ports, currentCount, allCount)
+	}
+
+	*ch <- struct{}{}
+	wg.Add(1)
+	go func() {
+		Mutex.Lock()
+		structs.AddScanNum += 1
+		Mutex.Unlock()
+		ScanFunc(&scantype, &info)
+		Mutex.Lock()
+		structs.AddScanEnd += 1
+		Mutex.Unlock()
+		wg.Done()
+		<-*ch
+	}()
+}
+
+func ScanFunc(name *string, info *structs.HostInfo) {
+	f := reflect.ValueOf(PluginList[*name])
+	in := []reflect.Value{reflect.ValueOf(info)}
+	f.Call(in)
+}
+
+var allCount = 0
+
+func GoPocsDispatcher(nucleiResults []output.ResultEvent) {
+	if len(structs.GlobalIPPortMap) == 0 && len(nucleiResults) == 0 {
+		return
+	}
+
+	initDic()
+
+	allCount = len(structs.GlobalIPPortMap) + len(nucleiResults)
+
+	var ch = make(chan struct{}, structs.GlobalConfig.GoPocThreads)
+	var wg = sync.WaitGroup{}
+	gologger.Info().Msg("Golang Poc引擎启动")
+
+	// 各类协议
+
+	for hostPort, protocol := range structs.GlobalIPPortMap {
+		t := strings.Split(hostPort, ":")
+		host := t[0]
+		port := t[1]
+
+		if protocol == "ssh" || port == "22" {
+			AddScan("SSH-Crack",
+				structs.HostInfo{Host: host, Ports: port},
+				&ch, &wg)
+		}
+		if protocol == "ftp" || port == "21" {
+			AddScan("FTP-Crack",
+				structs.HostInfo{Host: host, Ports: port},
+				&ch, &wg)
+		}
+		if protocol == "mysql" || port == "3306" {
+			AddScan("Mysql-Crack",
+				structs.HostInfo{Host: host, Ports: port},
+				&ch, &wg)
+		}
+		if protocol == "mssql" || port == "1433" {
+			AddScan("Mssql-Crack",
+				structs.HostInfo{Host: host, Ports: port},
+				&ch, &wg)
+		}
+		if protocol == "oracle" || port == "1521" {
+			AddScan("Oracle-Crack",
+				structs.HostInfo{Host: host, Ports: port},
+				&ch, &wg)
+		}
+		if protocol == "mongodb" || port == "27017" {
+			AddScan("MongoDB-Crack",
+				structs.HostInfo{Host: host, Ports: port},
+				&ch, &wg)
+		}
+		if protocol == "rdp" || port == "3389" {
+			AddScan("RDP-Crack",
+				structs.HostInfo{Host: host, Ports: port},
+				&ch, &wg)
+		}
+		if protocol == "redis" || port == "6379" {
+			AddScan("Redis-Crack",
+				structs.HostInfo{Host: host, Ports: port},
+				&ch, &wg)
+		}
+		if protocol == "smb" || port == "445" {
+			AddScan("SMB-MS17-010",
+				structs.HostInfo{Host: host, Ports: port},
+				&ch, &wg)
+			AddScan("SMB-Crack",
+				structs.HostInfo{Host: host, Ports: port},
+				&ch, &wg)
+		}
+		if protocol == "postgresql" || port == "5432" {
+			AddScan("PostgreSQL-Crack",
+				structs.HostInfo{Host: host, Ports: port},
+				&ch, &wg)
+		}
+		if protocol == "telnet" || port == "23" {
+			AddScan("Telnet-Crack",
+				structs.HostInfo{Host: host, Ports: port},
+				&ch, &wg)
+		}
+		if protocol == "memcached" || port == "11211" {
+			AddScan("Memcache-Crack",
+				structs.HostInfo{Host: host, Ports: port},
+				&ch, &wg)
+		}
+		if protocol == "netbios" || port == "445" {
+			AddScan("NetBios-GetHostInfo",
+				structs.HostInfo{Host: host, Ports: port},
+				&ch, &wg)
+		}
+		if protocol == "rpc" {
+			AddScan("RPC-GetHostInfo",
+				structs.HostInfo{Host: host, Ports: port},
+				&ch, &wg)
+		}
+		if protocol == "jdwp" {
+			AddScan("JDWP-Scan",
+				structs.HostInfo{Host: host, Ports: port},
+				&ch, &wg)
+		}
+		if protocol == "adb" || port == "5555" {
+			AddScan("ADB-Scan",
+				structs.HostInfo{Host: host, Ports: port},
+				&ch, &wg)
+		}
+
+	}
+
+	// 各类指纹
+	//for host, fingers := range structs.GlobalResultMap {
+	//
+	//}
+
+	for _, result := range nucleiResults {
+		if result.TemplateID == "shiro-detect" {
+			AddScan("Shiro-Key-Crack",
+				structs.HostInfo{Url: result.Matched},
+				&ch, &wg)
+		}
+	}
+
+	wg.Wait()
+}
---
+++ main.go
@@ -0,0 +1,294 @@
+package main
+
+import (
+	"dddd/common"
+	"dddd/common/callnuclei"
+	"dddd/common/http"
+	"dddd/common/report"
+	"dddd/common/uncover"
+	"dddd/gopocs"
+	"dddd/lib/ddfinger"
+	"dddd/structs"
+	"dddd/utils"
+	"dddd/utils/cdn"
+	"github.com/logrusorgru/aurora"
+	"github.com/projectdiscovery/gologger"
+	"github.com/projectdiscovery/httpx"
+	"github.com/projectdiscovery/nuclei/v3/pkg/output"
+	"strings"
+)
+
+func main() {
+	common.Flag()
+
+	workflow()
+}
+func workflow() {
+	var domains []string
+	var urls []string
+	var domainPort []string
+	var ipPort []string
+	var ips []string
+
+	defer gologger.Info().Msg(aurora.BrightGreen("Done!").String())
+
+	searchEngine()
+
+	for _, input := range structs.GlobalConfig.Targets {
+		inputType := utils.GetInputType(input)
+		if inputType == structs.TypeDomain {
+			domains = append(domains, input)
+			continue
+		} else if inputType == structs.TypeDomainPort {
+			domainPort = append(domainPort, input)
+			continue
+		} else if inputType == structs.TypeCIDR {
+			for _, ip := range utils.CIDRToIP(input) {
+				ips = append(ips, ip.String())
+			}
+		} else if inputType == structs.TypeIPRange {
+			for _, ip := range utils.RangerToIP(input) {
+				ips = append(ips, ip.String())
+			}
+		} else if inputType == structs.TypeIP {
+			ips = append(ips, input)
+		} else if inputType == structs.TypeIPPort {
+			ipPort = append(ipPort, input)
+		} else if inputType == structs.TypeURL {
+			urls = append(urls, input)
+		}
+	}
+
+	if structs.GlobalConfig.Subdomain && len(domains) > 0 {
+		subdomains := common.GetSubDomain(domains)
+		for _, each := range subdomains {
+			domains = append(domains, each)
+		}
+	}
+	domains = utils.RemoveDuplicateElement(domains)
+
+	var cdnDomains []string
+	var tIPs []string
+	if len(domains) > 0 {
+		cdnDomains, _, tIPs = cdn.CheckCDNs(domains, structs.GlobalConfig.SubdomainBruteForceThreads)
+		for _, each := range tIPs {
+			if structs.GlobalConfig.AllowLocalAreaDomain && utils.IsLocalIP(each) {
+				continue
+			}
+			ips = append(ips, each)
+		}
+	}
+	ips = utils.RemoveDuplicateElement(ips)
+
+	// 处理带CDN的域名，只进行https,http的探测，不进行端口扫描
+	if structs.GlobalConfig.AllowCDNAssets {
+		for _, cd := range cdnDomains {
+			urls = append(urls, "http://"+cd)
+			urls = append(urls, "https://"+cd)
+		}
+	}
+	urls = utils.RemoveDuplicateElement(urls)
+
+	// 端口扫描
+	if len(ips) > 0 {
+		if !structs.GlobalConfig.SkipHostDiscovery {
+			var ICMPAlive []string
+			// ICMP 探测存活
+			if !structs.GlobalConfig.NoICMPPing {
+				ICMPAlive = common.CheckLive(ips, false)
+			}
+
+			// TCP 探测存活
+			var TCPAlive []string
+			if structs.GlobalConfig.TCPPing {
+				// 获取没有存活的进行探测
+				var uncheck []string
+				for _, ip := range ips {
+					index := utils.GetItemInArray(ICMPAlive, ip)
+					if index == -1 {
+						uncheck = append(uncheck, ip)
+					}
+				}
+				gologger.Info().Msg("TCP存活探测")
+				common.PortScan = false
+				tcpAliveIPPort := common.PortScanTCP(uncheck, "80,443,3389,445,22",
+					structs.GlobalConfig.TCPPortScanTimeout)
+				for _, tIPPort := range tcpAliveIPPort {
+					t := strings.Split(tIPPort, ":")
+					TCPAlive = append(TCPAlive, t[0])
+				}
+			}
+
+			ips = append(ips, ICMPAlive...)
+			ips = append(ips, TCPAlive...)
+			ips = utils.RemoveDuplicateElement(ips)
+		}
+		var tmpIPPort []string
+
+		// 检测Masscan安装
+		if structs.GlobalConfig.PortScanType == "syn" {
+			if !common.CheckMasScan() {
+				gologger.Error().Msg("降级TCP扫描")
+				structs.GlobalConfig.PortScanType = "tcp"
+			}
+		}
+
+		if structs.GlobalConfig.PortScanType == "syn" {
+			// 全端口扫描
+			tmpIPPort = common.PortScanSYN(ips)
+		} else {
+			common.PortScan = true
+			tmpIPPort = common.PortScanTCP(ips, structs.GlobalConfig.Ports,
+				structs.GlobalConfig.TCPPortScanTimeout)
+		}
+
+		// 单个IP阈值过滤
+		tmpIPPort = common.RemoveFirewall(tmpIPPort)
+
+		for _, each := range tmpIPPort {
+			ipPort = append(ipPort, each)
+		}
+		ipPort = utils.RemoveDuplicateElement(ipPort)
+	}
+
+	getProtocalInput := ipPort
+	for _, each := range domainPort {
+		getProtocalInput = append(getProtocalInput, each)
+	}
+	if len(getProtocalInput) > 0 {
+		common.GetProtocol(getProtocalInput,
+			structs.GlobalConfig.GetBannerThreads,
+			structs.GlobalConfig.GetBannerTimeout)
+	}
+
+	// 获取http响应
+	for hostPort, service := range structs.GlobalIPPortMap {
+		if service == "http" {
+			urls = append(urls, "http://"+hostPort)
+		} else if service == "https" {
+			urls = append(urls, "https://"+hostPort)
+		}
+	}
+	urls = utils.RemoveDuplicateElement(urls)
+
+	httpx.CallHTTPx(urls, http.UrlCallBack,
+		structs.GlobalConfig.HTTPProxy,
+		structs.GlobalConfig.WebThreads,
+		structs.GlobalConfig.WebTimeout)
+
+	// 非CDN域名 探测域名绑定资产
+	// 把只允许域名访问的资产扒拉出来
+	if !structs.GlobalConfig.NoHostBind {
+		common.HostBindCheck()
+	}
+
+	var aliveURLs []string
+	for rootURL, _ := range structs.GlobalURLMap {
+		aliveURLs = append(aliveURLs, rootURL)
+	}
+
+	// 模糊搜索Yaml Poc直接打
+	if structs.GlobalConfig.PocNameForSearch != "" {
+		gologger.AuditTimeLogger("模糊搜索Poc: %v", structs.GlobalConfig.PocNameForSearch)
+		TargetAndPocsName := make(map[string][]string)
+		for _, url := range aliveURLs {
+			TargetAndPocsName[url] = []string{}
+		}
+		report.GenerateHTMLReportHeader()
+		callnuclei.CallNuclei(TargetAndPocsName,
+			structs.GlobalConfig.HTTPProxy,
+			report.AddResultByResultEvent,
+			structs.GlobalConfig.PocNameForSearch,
+			structs.GlobalConfig.NoInteractsh,
+			structs.GlobalEmbedPocs, structs.GlobalConfig.NucleiTemplate, strings.Split(structs.GlobalConfig.ExcludeTags, ","),
+			strings.Split(structs.GlobalConfig.Severities, ","))
+		utils.DeleteReportWithNoResult()
+		return
+	}
+
+	// 目录爆破
+	if !structs.GlobalConfig.NoDirSearch {
+		var checkURLs []string
+		for path, _ := range structs.DirDB {
+			for _, u := range aliveURLs {
+				Url := ""
+				if u[len(u)-1:] == "/" && path[0:1] == "/" {
+					Url = u[:len(u)-1] + path
+				} else {
+					Url = u + path
+				}
+				checkURLs = append(checkURLs, Url)
+			}
+		}
+		checkURLs = utils.RemoveDuplicateElement(checkURLs)
+		gologger.Info().Msg("开始主动指纹探测")
+		httpx.DirBrute(checkURLs,
+			http.DirBruteCallBack,
+			structs.GlobalConfig.HTTPProxy,
+			structs.GlobalConfig.WebThreads,
+			structs.GlobalConfig.WebTimeout)
+		gologger.AuditTimeLogger("主动指纹探测结束")
+	}
+
+	ddfinger.FingerprintIdentification()
+
+	if structs.GlobalConfig.NoPoc {
+		gologger.Info().Msg("跳过漏洞探测")
+		return
+	}
+
+	// 生成报告头部
+	report.GenerateHTMLReportHeader()
+
+	// 调用Nuclei
+	var nucleiResults []output.ResultEvent
+	TargetAndPocsName, count := http.GetPocs(structs.WorkFlowDB)
+	if count > 0 {
+		nucleiResults = callnuclei.CallNuclei(TargetAndPocsName,
+			structs.GlobalConfig.HTTPProxy,
+			report.AddResultByResultEvent,
+			"", structs.GlobalConfig.NoInteractsh, structs.GlobalEmbedPocs,
+			structs.GlobalConfig.NucleiTemplate, strings.Split(structs.GlobalConfig.ExcludeTags, ","),
+			strings.Split(structs.GlobalConfig.Severities, ","))
+	}
+
+	// GoPoc引擎
+	if !structs.GlobalConfig.NoGolangPoc {
+		gopocs.GoPocsDispatcher(nucleiResults)
+	}
+
+	// 没有漏洞结果，删除生成的HTML
+	utils.DeleteReportWithNoResult()
+
+}
+
+func searchEngine() {
+	// 从Hunter中获取资产
+	if structs.GlobalConfig.Hunter && !structs.GlobalConfig.Fofa {
+		structs.GlobalConfig.Targets, _ = uncover.HunterSearch(structs.GlobalConfig.Targets)
+		return
+	}
+	// 从Fofa中获取资产
+	if structs.GlobalConfig.Fofa && !structs.GlobalConfig.Hunter {
+		structs.GlobalConfig.Targets = uncover.FOFASearch(structs.GlobalConfig.Targets)
+		return
+	}
+	// 从Hunter中获取资产后使用Fofa进行端口补充。
+	if structs.GlobalConfig.Fofa && structs.GlobalConfig.Hunter {
+		targets, tIPs := uncover.HunterSearch(structs.GlobalConfig.Targets)
+		var querys []string
+		for _, i := range tIPs {
+			querys = append(querys, "ip=\""+i+"\"")
+		}
+		querys = utils.RemoveDuplicateElement(querys)
+		structs.GlobalConfig.Targets = uncover.FOFASearch(querys)
+		structs.GlobalConfig.Targets = append(structs.GlobalConfig.Targets, targets...)
+		structs.GlobalConfig.Targets = utils.RemoveDuplicateElement(structs.GlobalConfig.Targets)
+		return
+	}
+	// 从Quake获取资产
+	if structs.GlobalConfig.Quake {
+		structs.GlobalConfig.Targets = uncover.QuakeSearch(structs.GlobalConfig.Targets)
+	}
+
+}
